346938afda1eb5e02aab7fd192795ef068840230
hllojune <gy12wns200@gmail.com>
2021-11-10 18:16:02 +0900

    Third commit

---
+++ Git.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main(){
+    printf("Hello GitHub World\n");
+    printf("Git, it's your first time\n");
+    printf("Thanks to CoAlNu for references\n");
+    //https://hackmd.io/@oW_dDxdsRoSpl0M64Tfg2g/ByfwpNJ-K
+}
