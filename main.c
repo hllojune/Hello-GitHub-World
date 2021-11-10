30a51982962eb816e2f9deed67650f0cf6af92fd
hllojune <gy12wns200@gmail.com>
2021-11-10 18:06:00 +0900

    Second commit

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
 void main(){
-    printf("Hello World!");
+    printf("Hello World!\n");
+    printf("Hello C compiler\n");
+    printf("Thanks to rasino iOT\n");
+    //https://rasino.tistory.com/307#google_vignette
 }
